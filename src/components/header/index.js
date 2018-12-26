f9f238f541a6ff17003e80b1f0f904dce25674b5
liutaochange <liutaochange@163.com>
2018-12-26 11:53:20 +0800

    header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
 class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: '黄老邪'
+    }
+  }
   render() {
     return (
-      <div>header</div>
+      <div className='header-wamp'>
+        <Row className='header-item'>
+          <Col span={24}>
+            <span>欢迎： {this.state.userName}</span>
+            <a href="javascript:void(0)">退出</a>
+          </Col>
+        </Row>
+        <Row className='header-item-wea'>
+          <Col span={4}>首页</Col>
+          <Col span={20}>
+            <span>2018-12-26</span>
+            <span>晴转多云</span>
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
