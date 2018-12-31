a83cb59f6cebd1d4c78959849fc5e93df73deb17
liutaochange <liutaochange@163.com>
2018-12-31 22:42:36 +0800

    ui button

--- src/pages/ui/button/index.js
+++ src/pages/ui/button/index.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import { Card, Button } from 'antd';
-import styles from './style.module.less'
+import React, { Component } from "react";
+import { Card, Button } from "antd";
+import styles from "./style.module.less";
 class Buttons extends Component {
   render() {
     return (
       <div className={styles.buttonwamp}>
-        <Card title="基础按钮">
+        <Card title="基础按钮" style={{marginBottom: '10px'}}>
           <Button type="primary">Primary</Button>
           <Button>Default</Button>
           <Button type="dashed">Dashed</Button>
           <Button type="danger">Danger</Button>
         </Card>
+        <Card title="带图标按钮">
+          <Button type="primary" shape="circle" icon="search" />
+          <Button type="primary" icon="search">Search</Button>
+          <Button shape="circle" icon="search" />
+          <Button icon="search">Search</Button>
+        </Card>
       </div>
-    )
+    );
   }
 }
 export default Buttons;
