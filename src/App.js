5efc8dd1739d03b2368c4e0f9ea662de26c916f2
liutaochange <liutaochange@163.com>
2019-01-17 10:38:15 +0800

    fix router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, lazy, Suspense } from 'react';
 import { Layout } from 'antd';
 import LeftNav from '@/components/leftnav/index.js';
 import Header from '@/components/header/index.js';
 import Footer from '@/components/footer/index.js';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Loading from '@/components/loading/index.js';
 import './App.less';
+const Home = lazy(() => import('@/pages/home/index.js'));
+const Button = lazy(() => import('@/pages/ui/button/index.js'));
+const Modal = lazy(() => import('@/pages/ui/modal/index.js'));
+const Spin = lazy(() => import('@/pages/ui/spin/index.js'));
+const Notification = lazy(() => import('@/pages/ui/notification/index.js'));
+const Message = lazy(() => import('@/pages/ui/message/index.js'));
 const { Sider, Content } = Layout;
 class Admin extends PureComponent {
   constructor(props) {
@@ -36,7 +44,17 @@ class Admin extends PureComponent {
               margin: '24px 16px', padding: 24, background: '#fff', overflow: 'hidden', boxSizing: 'border-box', position: 'relative'
             }}
             >
-            {this.props.children}
+            <Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/home" component={props => <Home {...props} />} />
+                <Route exact path="/ui/buttons" component={props => <Button {...props} />} />
+                <Route exact path="/ui/modals" component={props => <Modal {...props} />} />
+                <Route exact path="/ui/loadings" component={props => <Spin {...props} />} />
+                <Route exact path="/ui/notification" component={props => <Notification {...props} />} />
+                <Route exact path="/ui/messages" component={props => <Message {...props} />} />
+                <Redirect to="home" />
+              </Switch>
+            </Suspense>
             </Content>
             <Footer />
           </Layout>
--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,11 @@ import Loading from '@/components/loading/index.js';
 const Admin = lazy(() => import('@/App.js'));
 const Login = lazy(() => import('@/pages/login/index.js'));
 const Nomatch = lazy(() => import('@/pages/nomatch/index.js'));
-const Home = lazy(() => import('@/pages/home/index.js'));
-const Button = lazy(() => import('@/pages/ui/button/index.js'));
-const Modal = lazy(() => import('@/pages/ui/modal/index.js'));
-const Spin = lazy(() => import('@/pages/ui/spin/index.js'));
-const Notification = lazy(() => import('@/pages/ui/notification/index.js'));
-const Message = lazy(() => import('@/pages/ui/message/index.js'));
 const Routers = () => (
   <Router>
     <Suspense fallback={<Loading />}>
       <Switch>
-        <Route path="/" component={props => <Admin {...props} />}>
-          <Switch>
-            <Route exact path="home" component={props => <Home {...props} />} />
-            <Route exact path="ui/buttons" component={props => <Button {...props} />} />
-            <Route exact path="ui/modals" component={props => <Modal {...props} />} />
-            <Route exact path="ui/loadings" component={props => <Spin {...props} />} />
-            <Route exact path="ui/notification" component={props => <Notification {...props} />} />
-            <Route exact path="ui/messages" component={props => <Message {...props} />} />
-            <Redirect to="home" />
-          </Switch>
-        </Route>
+        <Route path="/" component={props => <Admin {...props} />}></Route>
         <Route exact path="/login" component={props => <Login {...props} />} />
         <Route component={props => <Nomatch {...props} />} />
       </Switch>
