a4e29008f91184e2ab9d24dbb9ea37e6feb449ce
liutaochange <liutaochange@163.com>
2019-01-17 14:08:35 +0800

    form

---
+++ src/pages/form/login/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import style from './style.module.less'
+class Login extends PureComponent {
+  render() {
+    return (
+      <div className={style.login}>
+        this is login
+      </div>
+    )
+  }
+}
+export default Login;
