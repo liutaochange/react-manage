2de538e5a51e9c1b7eba4e515620af99040d63d0
liutaochange <liutaochange@163.com>
2019-03-03 22:08:23 +0800

    form

--- src/pages/form/login/index.js
+++ src/pages/form/login/index.js
@@ -1,10 +1,24 @@
 import React, { PureComponent } from 'react';
-import style from './style.module.less'
+import { Card, Form, Input, Button } from "antd";
+import style from './style.module.less';
+const FormItem = Form.Item;
 class Login extends PureComponent {
   render() {
     return (
       <div className={style.login}>
-        this is login
+        <Card title="行内内联表单">
+          <Form layout="inline">
+            <FormItem>
+              <Input placeholder="请输入用户名" type="text" />
+            </FormItem>
+            <FormItem>
+              <Input placeholder="请输入密码" type="password" />
+            </FormItem>
+            <FormItem>
+              <Button type="primary">提交</Button>
+            </FormItem>
+          </Form>
+        </Card>
       </div>
     )
   }
